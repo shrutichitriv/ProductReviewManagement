e8f3b006ecb2b4922c0b9b768f0da9ec25a74973
shrutichitriv <shrutichitriv199620@gmail.com>
2023-02-23 16:25:47 +0530

    [Shruti] Perform Product review management system

---
+++ LinqProblems/Program.cs
@@ -0,0 +1,12 @@
+﻿
+namespace LinqProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Product Review Management System");
+            
+        }
+    }
+}
